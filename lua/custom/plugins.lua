df2a65dad4c3742f0c7c873c498cc02312d81c96
jack-chapman <jackchapman093@gmail.com>
2024-03-05 22:43:51 +0000

    Update Rust config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -20,14 +20,41 @@ local plugins = {
     end
   },
   {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
-      return require "custom.configs.rust-tools"
-    end,
-    config = function (_, opts)
-      require('rust-tools').setup(opts)
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      {
+        "lvimuser/lsp-inlayhints.nvim",
+        opts = {}
+      }
+    },
+    ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        inlay_hints = {
+          highlight = "NonText",
+        },
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+          },
+        },
+        server = {
+          on_attach = function(client, bufnr)
+            require('custom.configs.rust-tools').server.on_attach(client, bufnr)
+            require('lsp-inlayhints').on_attach(client, bufnr)
+
+            local wk = require('which-key')
+            wk.register({
+              -- ['<leader>ca'] = { function() vim.cmd.RustLsp("codeAction") end, "Code Action" },
+              ['<leader>ta'] = { function() vim.cmd.RustLsp("testables") end, "Rust testables" },
+            }, { mode = "n", buffer = bufnr })
+          end,
+          capabilities = require('custom.configs.rust-tools').server.capabilities
+        }
+      }
     end
   }
 }
